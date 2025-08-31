3d5325a73c20b374b01c495050919f377d564283
beastocodez <myselfadarsh1463@gmail.com>
2025-08-31 11:51:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.API_KEY = "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";
+window.API_KEY = "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";
